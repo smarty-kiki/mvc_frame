8d44e593c2ddc15566c43664181209cb6a1db6d4
yaoyang <kiki@smarty.so>
2019-03-10 18:01:20 +0800

    开发环境映射性能监控平台

--- project/tool/start_dev_server.sh
+++ project/tool/start_dev_server.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../../
+ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../..
 
 sh $ROOT_DIR/project/tool/dep_build.sh link
 
-sudo docker run --rm -ti -p 80:80 -p 3306:3306 --name mvc_frame \
+sudo docker run --rm -ti -p 80:80 -p 8080:8080 -p 3306:3306 --name mvc_frame \
     -v $ROOT_DIR/../frame:/var/www/frame \
-    -v $ROOT_DIR:/var/www/mvc_frame \
+    -v $ROOT_DIR/:/var/www/mvc_frame \
     -v $ROOT_DIR/project/config/development/nginx/mvc_frame.conf:/etc/nginx/sites-enabled/default \
     -v $ROOT_DIR/project/config/development/supervisor/queue_worker.conf:/etc/supervisor/conf.d/queue_worker.conf \
+    -e 'TIMEZONE=Asia/Shanghai' \
 kikiyao/debian_php_dev_env start
