99768570263ddc2b46f70df918d6f787bb26b887
yaoyang <kiki@smarty.so>
2019-04-29 11:12:03 +0800

    支持 frame 的 midware 升级

--- config/beanstalk.php
+++ config/beanstalk.php
@@ -1,9 +1,15 @@
 <?php
 
 return [
-    'default' => [
-        'host' => '127.0.0.1',
-        'port' => 11300,
-        'timeout' => 1,
+    'midwares' => [
+        'default' => 'local',
+    ],
+
+    'resources' => [
+        'local' => [
+            'host' => '127.0.0.1',
+            'port' => 11300,
+            'timeout' => 1,
+        ],
     ],
 ];
