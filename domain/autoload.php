d124b405d122a004aba7c70cc1cd1702d3854ec6
yaoyang <kiki@smarty.so>
2019-08-19 00:07:43 +0800

    修改 classmap 中数据为新写法

--- domain/autoload.php
+++ domain/autoload.php
@@ -2,10 +2,10 @@
 
 spl_autoload_register(function ($class_name) {
 
-    $class_maps = array(
-    );
+    $class_maps = [
+    ];
 
     if (isset($class_maps[$class_name])) {
-         include __DIR__.'/'.$class_maps[$class_name];
+        include __DIR__.'/'.$class_maps[$class_name];
     }
 });
