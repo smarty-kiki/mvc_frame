fb46b17b09e67960a5a35f28c71177f876605e6c
yaoyang <kiki@smarty.so>
2019-12-16 22:36:36 +0800

    debug 当删除实体 description 文件时实体和 dao 不能被删除的问题

--- project/tool/fast_demo_watch.sh
+++ project/tool/fast_demo_watch.sh
@@ -11,70 +11,73 @@ then
     then
         echo $$ > $LOCK_FILE
         (
-            if [ "$filenames" = ".relationship.yml" ]
-            then
-                filenames=`ls $PROJECT_PATH/domain/description/`
-                event="MODIFY"
+        if [ "$filenames" = ".relationship.yml" ]
+        then
+            filenames=`ls $PROJECT_PATH/domain/description/`
+            event="MODIFY"
+        fi
+
+        for filename in $filenames
+        do
+            entity_name=${filename%.*}
+
+            ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php migrate:reset
+
+            rm -rf $PROJECT_PATH/view/$entity_name
+            #rm -rf $PROJECT_PATH/domain/dao/$entity_name.php
+            #rm -rf $PROJECT_PATH/domain/entity/$entity_name.php
+            rm -rf $PROJECT_PATH/command/migration/*_$entity_name.sql
+            rm -rf $PROJECT_PATH/controller/$entity_name.php
+
+            grep -v "'\/$entity_name\." $PROJECT_PATH/public/index.php > /tmp/index.php
+            mv /tmp/index.php $PROJECT_PATH/public/index.php
+
+            grep -v "'$entity_name'" $PROJECT_PATH/controller/index.php > /tmp/controller_index.php
+            mv /tmp/controller_index.php $PROJECT_PATH/controller/index.php
+
+            if [ "$event" = "CREATE" ];then
+                echo $filename $event
+
+                ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php entity:make-from-description --entity_name=$entity_name
+                /bin/bash $PROJECT_PATH/project/tool/classmap.sh $PROJECT_PATH/domain
+                ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php migrate
+
+                ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php crud:make-from-description --entity_name=$entity_name
+                /bin/sed -i "/init\ controller/a\include\ CONTROLLER_DIR\.\'\/$entity_name\.php\'\;" $PROJECT_PATH/public/index.php
+
+                list_url=`cat $PROJECT_PATH/controller/$entity_name.php | head -n 3 | tail -n 1 | cut -d "'" -f 2`
+                menu_name=`cat $PROJECT_PATH/domain/description/$entity_name.yml | head -n 2 | tail -n 1 | cut -d ' ' -f 2`
+                /bin/sed -i "/children/a\\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ [\ 'name'\ =>\ \'$menu_name管理\',\ \'key\'\ =>\ \'$entity_name\',\ \'href\'\ =>\ \'$list_url\',\ ]," $PROJECT_PATH/controller/index.php
             fi
 
-            for filename in $filenames
-            do
-                entity_name=${filename%.*}
+            if [ "$event" = "MODIFY" ];then
+                echo $filename $event
 
-                ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php migrate:reset
+                ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php entity:make-from-description --entity_name=$entity_name
+                /bin/bash $PROJECT_PATH/project/tool/classmap.sh $PROJECT_PATH/domain
+                ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php migrate
 
-                rm -rf $PROJECT_PATH/view/$entity_name
-                #rm -rf $PROJECT_PATH/domain/dao/$entity_name.php
-                #rm -rf $PROJECT_PATH/domain/entity/$entity_name.php
-                rm -rf $PROJECT_PATH/command/migration/*_$entity_name.sql
-                rm -rf $PROJECT_PATH/controller/$entity_name.php
+                ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php crud:make-from-description --entity_name=$entity_name
+                /bin/sed -i "/init\ controller/a\include\ CONTROLLER_DIR\.\'\/$entity_name\.php\'\;" $PROJECT_PATH/public/index.php
 
-                grep -v "'\/$entity_name\." $PROJECT_PATH/public/index.php > /tmp/index.php
-                mv /tmp/index.php $PROJECT_PATH/public/index.php
+                list_url=`cat $PROJECT_PATH/controller/$entity_name.php | head -n 3 | tail -n 1 | cut -d "'" -f 2`
+                menu_name=`cat $PROJECT_PATH/domain/description/$entity_name.yml | head -n 2 | tail -n 1 | cut -d ' ' -f 2`
+                /bin/sed -i "/children/a\\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ [\ 'name'\ =>\ \'$menu_name管理\',\ \'key\'\ =>\ \'$entity_name\',\ \'href\'\ =>\ \'$list_url\',\ ]," $PROJECT_PATH/controller/index.php
+            fi
 
-                grep -v "'$entity_name'" $PROJECT_PATH/controller/index.php > /tmp/controller_index.php
-                mv /tmp/controller_index.php $PROJECT_PATH/controller/index.php
+            if [ "$event" = "DELETE" ];then
+                echo $filename $event
 
-                if [ "$event" = "CREATE" ];then
-                    echo $filename $event
+                rm -rf $PROJECT_PATH/domain/dao/$entity_name.php
+                rm -rf $PROJECT_PATH/domain/entity/$entity_name.php
 
-                    ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php entity:make-from-description --entity_name=$entity_name
-                    /bin/bash $PROJECT_PATH/project/tool/classmap.sh $PROJECT_PATH/domain
-                    ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php migrate
+                /bin/bash $PROJECT_PATH/project/tool/classmap.sh $PROJECT_PATH/domain
+                ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php migrate
+            fi
 
-                    ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php crud:make-from-description --entity_name=$entity_name
-                    /bin/sed -i "/init\ controller/a\include\ CONTROLLER_DIR\.\'\/$entity_name\.php\'\;" $PROJECT_PATH/public/index.php
+        done
 
-                    list_url=`cat $PROJECT_PATH/controller/$entity_name.php | head -n 3 | tail -n 1 | cut -d "'" -f 2`
-                    menu_name=`cat $PROJECT_PATH/domain/description/$entity_name.yml | head -n 2 | tail -n 1 | cut -d ' ' -f 2`
-                    /bin/sed -i "/children/a\\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ [\ 'name'\ =>\ \'$menu_name管理\',\ \'key\'\ =>\ \'$entity_name\',\ \'href\'\ =>\ \'$list_url\',\ ]," $PROJECT_PATH/controller/index.php
-                fi
-
-                if [ "$event" = "MODIFY" ];then
-                    echo $filename $event
-
-                    ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php entity:make-from-description --entity_name=$entity_name
-                    /bin/bash $PROJECT_PATH/project/tool/classmap.sh $PROJECT_PATH/domain
-                    ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php migrate
-
-                    ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php crud:make-from-description --entity_name=$entity_name
-                    /bin/sed -i "/init\ controller/a\include\ CONTROLLER_DIR\.\'\/$entity_name\.php\'\;" $PROJECT_PATH/public/index.php
-
-                    list_url=`cat $PROJECT_PATH/controller/$entity_name.php | head -n 3 | tail -n 1 | cut -d "'" -f 2`
-                    menu_name=`cat $PROJECT_PATH/domain/description/$entity_name.yml | head -n 2 | tail -n 1 | cut -d ' ' -f 2`
-                    /bin/sed -i "/children/a\\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ [\ 'name'\ =>\ \'$menu_name管理\',\ \'key\'\ =>\ \'$entity_name\',\ \'href\'\ =>\ \'$list_url\',\ ]," $PROJECT_PATH/controller/index.php
-                fi
-
-                if [ "$event" = "DELETE" ];then
-                    echo $filename $event
-
-                    /bin/bash $PROJECT_PATH/project/tool/classmap.sh $PROJECT_PATH/domain
-                    ENV=$env /usr/bin/php $PROJECT_PATH/public/cli.php migrate
-                fi
-
-            done
-
-            rm -rf $LOCK_FILE
+        rm -rf $LOCK_FILE
         ) &
     fi
 fi
