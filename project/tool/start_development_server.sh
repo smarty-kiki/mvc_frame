4fec31094d8013a2ded6364de79830bab21fc909
yaoyang <kiki@smarty.so>
2020-06-12 13:51:25 +0800

    启动镜像时添加env，启动时的tmux window6 可以默认跳到项目目录

--- project/tool/start_development_server.sh
+++ project/tool/start_development_server.sh
@@ -9,6 +9,8 @@ sudo docker run --rm -ti -p 80:80 -p 8080:8080 -p 3306:3306 --name mvc_frame \
     -v $ROOT_DIR/:/var/www/mvc_frame \
     -v $ROOT_DIR/project/config/development/nginx/mvc_frame.conf:/etc/nginx/sites-enabled/default \
     -v $ROOT_DIR/project/config/development/supervisor/queue_worker.conf:/etc/supervisor/conf.d/queue_worker.conf \
+    -e 'PRJ_HOME=/var/www/mvc_frame' \
+    -e 'ENV=development' \
     -e 'TIMEZONE=Asia/Shanghai' \
     -e 'AFTER_START_SHELL=/var/www/mvc_frame/project/tool/development/after_env_start.sh' \
 kikiyao/debian_php_dev_env start
