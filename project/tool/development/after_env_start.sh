20897dcea140256a80bd18bd6557c253e980bdf7
yaoyang <kiki@smarty.so>
2020-12-27 13:21:28 +0800

    同步 api_frame 的修改

--- project/tool/development/after_env_start.sh
+++ project/tool/development/after_env_start.sh
@@ -2,5 +2,10 @@
 
 ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../../..
 
+mysql -e "create database \`default\`;\
+    GRANT ALL PRIVILEGES ON *.* TO 'default'@'%' IDENTIFIED BY 'password';\
+    GRANT ALL PRIVILEGES ON *.* TO 'default'@'localhost' IDENTIFIED BY 'password';\
+    FLUSH PRIVILEGES"
+
 ENV=development php $ROOT_DIR/public/cli.php migrate:install
 ENV=development php $ROOT_DIR/public/cli.php migrate
