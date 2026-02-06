dea9129571f1cafc5dd512f2cdade1f28814c7e1
kiki <378031327@qq.com>
2026-02-07 00:13:46 +0800

    添加 demo 实体

--- domain/autoload.php
+++ domain/autoload.php
@@ -3,6 +3,8 @@
 spl_autoload_register(function ($class_name) {
 
     $class_maps = [
+        'demo_dao' => 'dao/demo.php',
+        'demo' => 'entity/demo.php',
     ];
 
     if (isset($class_maps[$class_name])) {
---
+++ domain/entity/demo.php
@@ -0,0 +1,35 @@
+<?php
+
+class demo extends entity
+{
+    /* generated code start */
+    public $structs = [
+        'name' => '',
+        'note' => '',
+    ];
+
+    public static $struct_data_types = [
+        'name' => 'string',
+        'note' => 'string',
+    ];
+
+    public static $struct_display_names = [
+        'name' => '名称',
+        'note' => '备注',
+    ];
+
+    public static $struct_is_required = [
+        'name' => true,
+        'note' => false,
+    ];
+
+    public static function create($name): demo
+    /*{{{*/
+        $demo = parent::init();
+
+        $demo->name = $name;
+
+        return $demo;
+    /*}}}*/
+    /* generated code end */
+}
